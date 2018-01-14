d2bed5ac0d83670e55be64136e4bec283a447b48
mgamanunes <mgamanunes@gmail.com>
2018-01-14 17:36:23 +0100

    Addressed warnings

--- mmga.sol
+++ mmga.sol
@@ -17,7 +17,7 @@ contract MMGA {
 
   struct SubComment {
     bytes32 textHash;
-    //In case comment refers to specific passage. begIndex should be set to -1 in case it doesn't
+    //In case comment refers to a specific passage. begIndex should be set to -1 in case it doesn't
     int32 begIndex;
     uint256 endIndex;
   }
@@ -46,12 +46,12 @@ contract MMGA {
   Comment[] public comments;
   SubComment[] public subComments;
 
-  function MMGA() {
+  function MMGA() public {
     contractOwner = msg.sender;
   }
 
 
-  function addArticle(int32 myTimestamp, bytes32 myUriHash, bytes32 myTitleHash, bytes32 myAuthorHash) {
+  function addArticle(int32 myTimestamp, bytes32 myUriHash, bytes32 myTitleHash, bytes32 myAuthorHash) public {
     require(!articleExists(myUriHash, myTimestamp));
 
     articles.push(Article({
@@ -68,7 +68,7 @@ contract MMGA {
 
   }
 
-  function addArticle2(bytes32 myCreator, int32 myTimestamp, bytes32 myUriHash, bytes32 myTitleHash, bytes32 myAuthorHash) {
+  function addArticle2(bytes32 myCreator, int32 myTimestamp, bytes32 myUriHash, bytes32 myTitleHash, bytes32 myAuthorHash) public {
     require(!articleExists(myUriHash, myTimestamp));
 
     articles.push(Article({
@@ -84,19 +84,19 @@ contract MMGA {
       }));
 
   }
-
-   function addAuthor(string name) {
+    
+   function addAuthor(string name) public {
       require(!authorExists(name));
       authors.push(name);
   }
 
-  function setAuthorInArticle(uint articleIndex, uint16 authorIndex) {
+  function setAuthorInArticle(uint articleIndex, uint16 authorIndex) public {
     //Author can only be set by the creator of the article
     require(msg.sender == article.creatorDomain);
     require(articleIndex < articles.length);
     require(authorIndex < authors.length);
 
-    Article article = articles[articleIndex];
+    Article storage article = articles[articleIndex];
     //Author can only be set if no comments have been done yet
     require(article.comments.length == 0);
 
@@ -105,11 +105,11 @@ contract MMGA {
 
   }
 
-  function removeAuthorFromArticle(uint articleIndex) {
+  function removeAuthorFromArticle(uint articleIndex) public {
     require(msg.sender == article.creatorDomain);
     require(articleIndex < articles.length);
 
-    Article article = articles[articleIndex];
+    Article storage article = articles[articleIndex];
     require(article.authorMapped == true);
     require(article.comments.length == 0);
 
@@ -119,14 +119,14 @@ contract MMGA {
 
 
   //begIndex = -1 means comment does not refer to passage in text
-  function addComment(uint articleIndex, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) {
+  function addComment(uint articleIndex, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) public {
     require(articleIndex < articles.length);
     require(myTextHash.length != 0);
     if(myBegIndex != -1) {
       require(uint(myBegIndex) < myEndIndex);
     }
 
-    Article article = articles[articleIndex];
+    Article storage article = articles[articleIndex];
     uint256 index = comments.length;
     comments.push(Comment({
         authorDomain: msg.sender,
@@ -142,7 +142,7 @@ contract MMGA {
   
   //begIndex = -1 means comment does not refer to passage in text
   //For domain comment (added on behalf of someone)
-  function addComment2(uint articleIndex, bytes32 myAuthorId, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) {
+  function addComment2(uint articleIndex, bytes32 myAuthorId, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) public {
     require(articleIndex < articles.length);
     require(myTextHash.length != 0);
     require(myAuthorId.length != 0);
@@ -150,7 +150,7 @@ contract MMGA {
       require(uint(myBegIndex) < myEndIndex);
     }
 
-    Article article = articles[articleIndex];
+    Article storage article = articles[articleIndex];
     uint256 index = comments.length;
     comments.push(Comment({
         authorDomain: msg.sender,
@@ -164,25 +164,7 @@ contract MMGA {
 
   }
   
-  function addNestedCommment(uint commentIndex, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) {
-    require(myTextHash.length != 0);
-    if(myBegIndex != -1) {
-      require(uint(myBegIndex) < myEndIndex);
-    }
-
-    require(commentIndex < comments.length);
-
-    uint256 index = subComments.length;
-    subComments.push(SubComment({
-        textHash: myTextHash,
-        begIndex: myBegIndex,
-        endIndex: myEndIndex
-      }));
-    comments[commentIndex].parts.push(index);
-
-  }
-
-  function addNestedCommment2(uint commentIndex, bytes32 myAuthorId, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) {
+  function addSubCommment(uint commentIndex, bytes32 myTextHash, int32 myBegIndex, uint32 myEndIndex) public {
     require(myTextHash.length != 0);
     if(myBegIndex != -1) {
       require(uint(myBegIndex) < myEndIndex);
@@ -201,7 +183,7 @@ contract MMGA {
   }
 
   //Returns -1 if author does not exist
-  function getAuthorIndexFromName(string name) constant returns (int) {
+  function getAuthorIndexFromName(string name) public constant returns (int) {
     for(uint i = 0; i < authors.length; i++) {
       if(keccak256(authors[i]) == keccak256(name)) {
         return int(i);
@@ -210,12 +192,12 @@ contract MMGA {
     return -1;
   }
 
-  function getAuthorNameFromIndex(uint ind) constant returns (string) {
+  function getAuthorNameFromIndex(uint ind) public constant returns (string) {
     require(ind < authors.length);
     return authors[ind];
   }
 
-  function authorExists(string name) constant returns (bool) {
+  function authorExists(string name) public constant returns (bool) {
     for(uint i = 0; i < authors.length; i++) {
       if(keccak256(authors[i]) == keccak256(name)) {
         return true;
@@ -226,7 +208,7 @@ contract MMGA {
 
   //Returns -1 if article does not exist
   //Gets next article index given possible hash colision
-  function getNextArticleIndex(uint start, bytes32 uriHash) constant returns (int) {
+  function getNextArticleIndex(uint start, bytes32 uriHash) public constant returns (int) {
     if(start >= articles.length) {
       return -1;
     }
@@ -238,7 +220,7 @@ contract MMGA {
     return -1;
   }
 
-  function articleExists(bytes32 uriHash, int32 timestamp) constant returns (bool) {
+  function articleExists(bytes32 uriHash, int32 timestamp) public constant returns (bool) {
     for(uint i = 0; i < articles.length; i++) {
       if(articles[i].uriHash == uriHash && articles[i].timestamp == timestamp) {
         return true;
@@ -247,40 +229,40 @@ contract MMGA {
     return false;
   }
 
-  function getContractOwner() constant returns (address) {
+  function getContractOwner() public constant returns (address) {
     return contractOwner;
   }
 
-  function getAuthorsLength() constant returns (uint256) {
+  function getAuthorsLength() public constant returns (uint256) {
     return authors.length;
   }
 
-  function getAuthor(uint256 index) constant returns (string) {
+  function getAuthor(uint256 index) public constant returns (string) {
     return authors[index];
   }
 
-  function getArticlesLength() constant returns (uint256) {
+  function getArticlesLength() public constant returns (uint256) {
     return articles.length;
   }
-  function getArticle(uint256 index) constant returns (address, bytes32, int32, bytes32, bytes32, bytes32, bool, uint, uint256[]) {
-    Article article = articles[index];
+  function getArticle(uint256 index) public constant returns (address, bytes32, int32, bytes32, bytes32, bytes32, bool, uint, uint256[]) {
+    Article storage article = articles[index];
     return (article.creatorDomain, article.creator, article.timestamp, article.uriHash, article.titleHash, 
       article.authorHash, article.authorMapped, article.authorIndex, article.comments);
   }
 
-  function getCommentsLength() constant returns (uint256) {
+  function getCommentsLength() public constant returns (uint256) {
     return comments.length;
   }
-  function getComment(uint256 index) constant returns (address, bytes32, bytes32, int32, uint32, uint256[]) {
-    Comment comment = comments[index];
+  function getComment(uint256 index) public constant returns (address, bytes32, bytes32, int32, uint32, uint256[]) {
+    Comment storage comment = comments[index];
     return (comment.authorDomain, comment.authorId, comment.textHash, comment.begIndex, comment.endIndex, comment.parts);
   }
 
-  function getSubCommentsLength() constant returns (uint256) {
+  function getSubCommentsLength() public constant returns (uint256) {
     return subComments.length;
   }
-  function getSubComment(uint256 index) constant returns (bytes32, int32, uint256) {
-    SubComment subComment = subComments[index];
+  function getSubComment(uint256 index) public constant returns (bytes32, int32, uint256) {
+    SubComment storage subComment = subComments[index];
     return (subComment.textHash, subComment.begIndex, subComment.endIndex);
   }
 }
